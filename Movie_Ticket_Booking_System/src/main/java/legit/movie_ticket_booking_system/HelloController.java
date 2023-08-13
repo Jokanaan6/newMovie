bcb9e948dbddf28db00e9b3f241891619e8e082a
Jokanaan6 <140316473+Jokanaan6@users.noreply.github.com>
2023-08-13 12:56:52 +0800

    Add files via upload
    new sruf

---
+++ Movie_Ticket_Booking_System/src/main/java/legit/movie_ticket_booking_system/HelloController.java
@@ -0,0 +1,14 @@
+package legit.movie_ticket_booking_system;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
